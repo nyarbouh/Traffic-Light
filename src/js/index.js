d54c1f7276de577439736f800ad05e93d0e716e5
nyarbouh <nyarbouh@me.com>
2019-09-16 07:43:07 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import PropTypes from "prop-types";
+
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-
-//import your own components
-import { Home } from "./component/home.js";
+import TrafficLight from "../js/TrafficLight";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
